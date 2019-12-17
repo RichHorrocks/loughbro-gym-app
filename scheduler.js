75f10538c78064b95c1bfa1a1b86751cda275f2b
Richard Horrocks <richardjhorrocks@gmail.com>
2019-12-17 13:10:21 +0000

    Move scheduler.

--- scheduler.js
+++ scheduler.js
@@ -1,5 +1,6 @@
-const db = require('../config/db');
-const scraper = require('./scraper');
+const db = require('./config/db');
+const scraper = require('./lib/scraper');
 
 // Connect to the database and run our scraper.
+console.log('HERE!');
 db.connectDB(() => scraper.runCron(() => db.disconnectDB()));
